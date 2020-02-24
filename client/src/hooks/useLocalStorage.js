2d3529932f472ea5fa2f6989c9acfb3d6100928d
Logan Negley <logannegley@gmail.com>
2020-02-23 20:02:19 -0600

    added styles to navbar for dark mode

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -2,13 +2,13 @@ import {useState} from 'react';
 
 export const useLocalStorage = (storageKey, initalStorageValue) => {
     const [windowStorage, setWindowStorage] = useState(() => {
-    const item = window.localStorage.getItem(key);
+    const item = window.localStorage.getItem(storageKey);
         return item ? JSON.parse(item) : initalStorageValue;
 });
 
 const setValue = value =>{
     setWindowStorage(value);
-    window.localStorage.setItem(key,JSON.stringify(value));
+    window.localStorage.setItem(storageKey,JSON.stringify(value));
 };
 
 return [windowStorage, setValue]
